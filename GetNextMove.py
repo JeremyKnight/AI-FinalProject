1bc7a85547a26d05ef222f8f8a16553bfe80a4a7
jknight22 <jknight22@my.whitworth.edu>
2022-05-13 12:42:28 -0700

    added evaluation class

--- GetNextMove.py
+++ GetNextMove.py
@@ -3,6 +3,7 @@ import random
 from sys import argv
 import chess
 from optparse import OptionParser
+from evaluation import evaluation
 
 def writeToFile(chessOutput):
     f = open('last_move.txt','w')
@@ -10,19 +11,41 @@ def writeToFile(chessOutput):
     f.close()
 
 class GetNextMove:
-    def __init__(self, fen, time_white, time_black, bonus_time):
-        print('fen from getNextMove init', fen)
+    def __init__(self, fen, time_white, time_black, bonus_time, color):
+        #print('fen from getNextMove init', fen)
         self.board = chess.Board(fen)
-        print(self.board)
+        #print(self.board)
         self.time_white = time_white
         self.time_black = time_black
         self.bonus_time = bonus_time
-        print("created a new GetNextMove")
+        self.color = color
+        #print("created a new GetNextMove")
     
     def nextMove(self):
         #chess.STARTING_FEN = self.board
-        print (self.board.legal_moves)
+        #print (self.board.legal_moves)
+        
+        
+
+        for i in range(1,100):
+            moves = list(self.board.legal_moves)
+            move = random.choice(moves)
+            self.board.push(move)
+            e = evaluation(self.board, self.color)
+            score = e.evaluate()
+            print("score from evaluation",score)
+            
+
+        #print(e)
         #print(self.board.legal_moves[1])
+        
+
+def runTest():
+    #print("hello there")
+    fenString = "rnb1kbnr/ppp2ppp/2p1qp2/8/8/3P1P2/PPP2PPP/RNBQKBNR w KQkq - 0 1"
+    
+    GM = GetNextMove(fenString,0,0,0, chess.WHITE)
+    GM.nextMove()
 
 if __name__ == "__main__":
     '''
@@ -31,18 +54,23 @@ if __name__ == "__main__":
     arg[3] = time_black
     arg[4] = bonus_time
     '''
-    fenString = argv[1]
+    #fenString = argv[1]
+    '''
+    fenString = chess.STARTING_FEN
     newBoard = chess.Board(fenString)
     moves = list(newBoard.legal_moves)
     move = random.choice(moves)
     writeToFile(move)
+    '''
+
+    runTest()
+    
     # print('start \n', newBoard)
     # newBoard.remove_piece_at(chess.A1)
     
     # print('new board',newBoard)
 
     # #GetNextMove GM = GetNextMove(argv[1], argv[2], argv[3], argv[4])
-    # GM = GetNextMove(newBoard.fen(),0,0,0)
-    # GM.nextMove()
+    
 
     #makeFile(options)
